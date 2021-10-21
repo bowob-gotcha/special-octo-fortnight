b21383cce500513024080286bfa1b4d14d7a23b6
bowob-gotcha <92845880+bowob-gotcha@users.noreply.github.com>
2021-10-21 10:36:23 +0800

    Update score.py

--- score.py
+++ score.py
@@ -1,3 +1,5 @@
+#You can either try this one
+
 score = input("Enter Score: ")
 x = float(score)
 
@@ -16,3 +18,27 @@ try:
         print("F")
 except ValueError:
     print("Not a number")
+    
+#Or this one
+
+score = input("Enter Score: ")
+
+try:
+    x = float(score)
+except:
+    x = -1
+    
+if x > 1.0 :
+    print("Out of Range")
+elif x >= 0.9 :
+    print("A")
+elif x >= 0.8 :
+    print("B")
+elif x >= 0.7 :
+    print("C")
+elif x >= 0.6 :
+    print("D")
+elif 0 < x < 0.6 :
+    print("F")
+else:
+    print("NOT A NUMBER")
